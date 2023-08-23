1630cfa69610ed56ccec9cde405a7595743965d9
Nayak <anuja.nayak@capgemini.com>
2023-08-23 15:36:15 +0530

    create Registration page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<RegisterPage />} />
+        <Route exact path="/LoginPage" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/registerPage.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./RegisterPage.css";
+
+function RegisterPage() {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const emailOnChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+  const registerbtn = () =>{
+navigate('/LoginPage')
+  }
+
+  return (
+    <div className="MainContainer">
+      <div className="FormContainer">
+        <div className="FormHeader">Registration Page</div>
+        <div className="FormInputs">
+          <label className="InputLabel">Name</label>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={handleName}
+          />
+          <label className="InputLabel">Email</label>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={emailOnChange}
+          />
+          <label className="InputLabel">Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={handlePassword}
+          />
+        </div>
+        <button onClick={registerbtn} style={{display:"flex",marginTop:"15px"}}>Register</button>
+      </div>
+      
+    </div>
+  );
+}
+
+export default RegisterPage;
